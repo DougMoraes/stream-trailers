c2362c533c4f9bce777ba35cef923f25ddc6511b
Douglas <dougmoraes91@gmail.com>
2020-05-23 21:07:36 +0200

    feat: implement apiCall to get most popular movies

--- src/Components/MoviesCarrousel/MoviesCarrousel.jsx
+++ src/Components/MoviesCarrousel/MoviesCarrousel.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Carousel from "react-material-ui-carousel";
-import MoviePoster from "../MoviePoster/MoviePoster"
+import { CircularProgress } from "@material-ui/core";
+import { apiKey, movieAPI } from "../../utils/APIs";
+import MoviePoster from "../MoviePoster/MoviePoster";
 
 const MoviesCarrousel = props => {
-  var items = [
-    {
-      name: "Random Name #1",
-      description: "Probably the most random thing you have ever seen!"
-    },
-    {
-      name: "Random Name #2",
-      description: "Hello World!"
-    }
-  ]
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    movieAPI
+      .get("/popular?api_key=" + apiKey)
+      .then(resp => resp.data)
+      .then(data => {
+        return splitArray(8, data.results);
+      })
+      .then(results => {
+        setPopularMovies(results);
+      });
+  }, []);
+
+  const splitArray = (itemsPerArray, array) => {
+    return new Array(Math.ceil(array.length / itemsPerArray))
+      .fill()
+      .map(() => array.splice(0, itemsPerArray));
+  };
+
+  const renderMoviesSets = (allMovies) => {
+    return (
+      <Carousel autoPlay={false}>
+        {
+          allMovies.map(moviesSet =>
+            moviesSet.map(movie =>
+              <MoviePoster
+                movieName={movie.original_title}
+                posterPath={movie.poster_path}
+              />
+            )
+          )
+        }
+      </Carousel>
+    )
+  }
 
   return (
-    <Carousel>
-      {
-        items.map(item => <MoviePoster />)
-      }
-    </Carousel>
-  )
-}
+    popularMovies.length !== 0 ? renderMoviesSets(popularMovies) : <CircularProgress />
+  );
+};
 
 export default MoviesCarrousel;
