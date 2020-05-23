6b2d762ba1660ad2bb50896f99f04fa0625751e1
Douglas <dougmoraes91@gmail.com>
2020-05-23 15:15:03 +0200

    feat: set fixed api call

--- src/Components/MoviePoster/MoviePoster.jsx
+++ src/Components/MoviePoster/MoviePoster.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { apiKey, movieAPI } from "../../utils/APIs";
 
 const MoviePoster = props => {
-  return <div>MoviePoster</div>;
-}
+  const [movieName, setMovieName] = useState("");
+
+  useEffect(() => {
+    movieAPI
+      .get("/550?api_key=" + apiKey)
+      .then(resp => resp.data)
+      .then(data => setMovieName(data.original_title));
+  }, []);
+
+  return <div>{movieName}</div>;
+};
 
 export default MoviePoster;
