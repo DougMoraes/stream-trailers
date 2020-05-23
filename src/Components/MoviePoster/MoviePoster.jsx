43eb040fa14179b153f89ec593df35a944c347d3
Douglas <dougmoraes91@gmail.com>
2020-05-23 18:01:21 +0200

    feat: add data-testid for movieName div

--- src/Components/MoviePoster/MoviePoster.jsx
+++ src/Components/MoviePoster/MoviePoster.jsx
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { CircularProgress } from "@material-ui/core";
 import { apiKey, movieAPI } from "../../utils/APIs";
-import './MoviePoster.scss';
+import "./MoviePoster.scss";
 
 const MoviePoster = props => {
   const [movieName, setMovieName] = useState("");
   const [posterPath, setPosterPath] = useState("");
 
   useEffect(() => {
-
     movieAPI
       .get("/550?api_key=" + apiKey)
       .then(resp => resp.data)
       .then(data => {
-        setMovieName(data.original_title)
-        setPosterPath(data.poster_path)
+        setMovieName(data.original_title);
+        setPosterPath(data.poster_path);
       });
   }, []);
 
-  return (
-    movieName === '' ? <CircularProgress /> :
-      <div className="movie-poster">
-        <img
-          src={`https://image.tmdb.org/t/p/w200${posterPath}`}
-          alt={`Poster for ${movieName} film`}
-        />
-        {movieName}
-      </div>
+  return movieName === "" ? (
+    <CircularProgress />
+  ) : (
+    <div className="movie-poster">
+      <img
+        src={`https://image.tmdb.org/t/p/w200${posterPath}`}
+        alt={`Poster for ${movieName} film`}
+      />
+      <div data-testid="movie-name">{movieName}</div>
+    </div>
   );
 };
 
