412ec4ebe7127cee64ef6c192c3b8bebba02b5aa
Douglas <dougmoraes91@gmail.com>
2020-05-24 21:06:16 +0200

    style: adjust video to occupy full modal space

--- src/Components/ItemDetails/ItemDetails.jsx
+++ src/Components/ItemDetails/ItemDetails.jsx
@@ -15,11 +15,9 @@ function getModalStyle() {
 const useStyles = makeStyles(theme => ({
   paper: {
     position: "absolute",
-    width: 400,
     backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3)
   }
 }));
 
@@ -62,8 +60,6 @@ const ItemDetails = props => {
       <Modal
         open={open}
         onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
       >
         {renderPlayer}
       </Modal>
