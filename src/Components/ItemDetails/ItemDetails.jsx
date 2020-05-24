010ec2f5920d3368e61e3522c57cdd6a2cc3a670
Douglas <dougmoraes91@gmail.com>
2020-05-24 15:56:12 +0200

    style: define page structure

--- src/Components/ItemDetails/ItemDetails.jsx
+++ src/Components/ItemDetails/ItemDetails.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import PlayCircleOutline from "@material-ui/icons/PlayCircleOutline";
+import'./ItemDetails.scss';
 
 const ItemDetails = props => {
-  const { title, overview, poster_path } = props.location.state.itemData;
+  const { title, overview, poster_path, vote_average } = props.location.state.itemData;
 
   return (
-    <div>
-      <h2>{title}</h2>
+    <div className="details-container">
+      <div>
+        <h2>{title}</h2>
+        <section>{overview}</section>
+        <section>{`Average Note: ${vote_average}`}</section>
+        <Button
+          startIcon={<PlayCircleOutline />}
+          variant="contained"
+          color="primary"
+        >
+          Watch the Trailer
+        </Button>
+      </div>
       <img
         src={`https://image.tmdb.org/t/p/w500${poster_path}`}
         alt={`Poster for ${title} film`}
       />
-      <section>{overview}</section>
     </div>
   );
 };
