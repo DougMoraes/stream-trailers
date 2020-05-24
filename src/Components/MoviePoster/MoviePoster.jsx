f6910215cde151737989f59d0e70c962504de25e
Douglas <dougmoraes91@gmail.com>
2020-05-24 19:05:12 +0200

    fix: fix series names

--- src/Components/MoviePoster/MoviePoster.jsx
+++ src/Components/MoviePoster/MoviePoster.jsx
@@ -3,16 +3,17 @@ import { Link } from "react-router-dom";
 import "./MoviePoster.scss";
 
 const MoviePoster = props => {
-  const { title, poster_path } = props.itemData
+  const { title, name, poster_path } = props.itemData
+  const itemName = title ? title : name;
 
   return (
-    <Link to={{ pathname: "/details", state: { itemData: props.itemData }}}>
+    <Link to={{ pathname: "/details", state: { itemData: props.itemData } }}>
       <div className="item-poster hvr-grow-shadow">
         <img
           src={`https://image.tmdb.org/t/p/w200${poster_path}`}
-          alt={`Poster for ${title} film`}
+          alt={`Poster for ${itemName} film`}
         />
-        <div className="item-name">{title}</div>
+        <div className="item-name">{itemName}</div>
       </div>
     </Link>
   );
