f448e54b1818a7b7c35914ab400133d89c842602
Douglas <dougmoraes91@gmail.com>
2020-05-24 15:22:14 +0200

    feat: add link on every component

--- src/Components/MoviePoster/MoviePoster.jsx
+++ src/Components/MoviePoster/MoviePoster.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./MoviePoster.scss";
 
 const MoviePoster = props => {
   return (
-    <div className="movie-poster hvr-grow-shadow">
-      <img
-        src={`https://image.tmdb.org/t/p/w200${props.posterPath}`}
-        alt={`Poster for ${props.movieName} film`}
-      />
-      <div data-testid="movie-name">{props.movieName}</div>
-    </div>
+    <Link to={{pathname: "/details", state: { props }}}>
+      <div className="movie-poster hvr-grow-shadow">
+        <img
+          src={`https://image.tmdb.org/t/p/w200${props.posterPath}`}
+          alt={`Poster for ${props.movieName} film`}
+        />
+        <div data-testid="movie-name">{props.movieName}</div>
+      </div>
+    </Link>
   );
 };
 
