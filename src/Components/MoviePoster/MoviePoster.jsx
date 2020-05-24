812b60fc147f0d82db44d16433c31a684a528715
Douglas <dougmoraes91@gmail.com>
2020-05-24 15:33:55 +0200

    refac: adequate item to receive all itemData

--- src/Components/MoviePoster/MoviePoster.jsx
+++ src/Components/MoviePoster/MoviePoster.jsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 import "./MoviePoster.scss";
 
 const MoviePoster = props => {
+  const { title, poster_path } = props.itemData
+
   return (
-    <Link to={{pathname: "/details", state: { props }}}>
+    <Link to={{ pathname: "/details", state: { itemData: props.itemData }}}>
       <div className="movie-poster hvr-grow-shadow">
         <img
-          src={`https://image.tmdb.org/t/p/w200${props.posterPath}`}
-          alt={`Poster for ${props.movieName} film`}
+          src={`https://image.tmdb.org/t/p/w200${poster_path}`}
+          alt={`Poster for ${title} film`}
         />
-        <div data-testid="movie-name">{props.movieName}</div>
+        <div data-testid="movie-name">{title}</div>
       </div>
     </Link>
   );
