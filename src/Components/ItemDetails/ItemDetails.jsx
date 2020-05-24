b1024ea937a73cdaa8c1d6e52452f530ac5ec676
Douglas <dougmoraes91@gmail.com>
2020-05-24 15:34:53 +0200

    feat: extract data from props

--- src/Components/ItemDetails/ItemDetails.jsx
+++ src/Components/ItemDetails/ItemDetails.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 const ItemDetails = props => {
-  console.log(props)
-  return <div>{props.location.state.props.movieName}</div>;
+  const { title, overview, poster_path } = props.location.state.itemData;
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        alt={`Poster for ${title} film`}
+      />
+      <section>{overview}</section>
+    </div>
+  );
 };
 
 export default ItemDetails;
