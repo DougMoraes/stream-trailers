6d8a0640643c9ea339824dca636fee52442dc3ac
Douglas <dougmoraes91@gmail.com>
2020-05-24 14:11:48 +0200

    feat: add different carousels to main page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,7 +6,14 @@ function App() {
   return (
     <div className="App">
       <h1>Stream Trailers App</h1>
-      <MoviesCarrousel />
+      <h2>Popular Movies</h2>
+      <MoviesCarrousel category="popular_movies" />
+      <h2>Popular Series</h2>
+      <MoviesCarrousel category="popular_series" />
+      <h2>Family Movies</h2>
+      <MoviesCarrousel category="family_movies" />
+      <h2>Documentaries</h2>
+      <MoviesCarrousel category="documentary" />
     </div>
   );
 }
